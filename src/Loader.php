f8899cdb2312883679f97e02437ac95b887a54a6
Dusan <dusan.daniska@wai.sk>
2023-11-13 13:17:06 +0100

    new components

--- src/Loader.php
+++ src/Loader.php
@@ -7,10 +7,12 @@ class Loader {
   public $twig;
   public array $options = [];
   public array $config = [];
+  public array $data = [];
 
-  public function __construct(array $options, array $config) {
+  public function __construct(array $options, array $config, $data) {
     $this->options = $options;
     $this->config = $config;
+    $this->data = $data;
 
     $this->twig = new \Twig\Environment(
       new \Twig\Loader\FilesystemLoader([__DIR__ . '/templates', (string) ($options['wireframesDir'] ?? '')]),
@@ -26,13 +28,17 @@ class Loader {
     $wireframe = $wireframe ?? 'index';
     $isWindow = (bool) ($this->options['isWindow'] ?? false);
 
-    $wireframeContentHtml = $this->twig->load($wireframe . '.twig')->render(['config' => $this->config]);
+    $wireframeContentHtml = $this->twig->load($wireframe . '.twig')->render([
+      'config' => $this->config,
+      'data' => $this->data,
+    ]);
 
     if ($isWindow) {
       $html = $wireframeContentHtml;
     } else {
       $html = $this->twig->load('desktop.twig')->render([
         'config' => $this->config,
+        'data' => $this->data,
         'desktopContentHtml' => $wireframeContentHtml,
       ]);
     }
